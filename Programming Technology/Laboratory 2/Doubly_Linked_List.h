d61e09ad6746943946948397dee6e741c76ebf36
Andrey Semenov <61043339+Lawranio@users.noreply.github.com>
2021-02-14 19:56:35 +0300

    Add files via upload

---
+++ Programming Technology/Laboratory 2/Doubly_Linked_List.h
@@ -0,0 +1,137 @@
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class DLL {
+public:
+	DLL() = default;
+	~DLL() {};
+	void pushBack(T data);
+	void pushFront(T data);
+	void erase(int index);
+	void Print();
+
+private:
+	class Element {
+	public:
+		T data{ 0 };					// поле данных
+		Element* nextptr{ nullptr };	// указатель на следующий элемент
+		Element* prevptr{ nullptr };	// указатель на предыдущий элемент
+		Element(T data, Element* nextptr = nullptr, Element* prevptr = nullptr) {
+			this->data = data;
+			this->nextptr = nextptr;
+			this->prevptr = prevptr;
+		}
+	};
+	int size{ 0 };						// размер даннных
+	Element* head{ nullptr };			// указатель на начало списка
+	Element* tail{ nullptr };			// указатель на конец списка
+};
+
+
+
+
+template <class T> void DLL<T>::erase(int index) {
+	try {
+		if ((index < 0) || (index >= size)) throw ("Error: Index is out of range");
+	}
+	catch (const char* error) {
+		cerr << error << endl;
+		exit(-2);
+	}
+
+	Element* temp;
+
+	if (index == 0) {
+		temp = head;
+		head = temp->nextptr;
+
+		delete temp;
+		size--;
+		return;
+	}
+
+	if (index == size - 1) {
+		Element* tempDelete = tail;
+		temp = tail;
+		temp = temp->prevptr;
+		temp->nextptr = nullptr;
+		tail = temp;
+
+		delete tempDelete;
+		size--;
+		return;
+	}
+
+	temp = head;
+	for (int i = 0; i < size; i++) {
+		if (i == index) {
+			cout << temp->data << endl;
+			Element* prev, * next;
+
+			prev = temp->prevptr;
+			next = temp->nextptr;
+
+			prev->nextptr = temp->nextptr;
+			next->prevptr = temp->prevptr;
+
+			delete temp;
+			size--;
+			return;
+		}
+		temp = temp->nextptr;
+	}
+	
+}
+
+
+template <class T> void DLL<T>::pushFront(T data) {
+	if (head == nullptr) {
+		head = new Element(data);
+		tail = head;
+	}
+	else {
+		Element* temp = head;
+		head = new Element(data, temp);
+		temp->prevptr = head;
+	}
+	size++;
+}
+
+
+template <class T> void DLL<T>::pushBack(T data) {
+	if (head == nullptr) {
+		head = new Element(data, head, tail);
+		tail = head;
+	}
+	else {
+		Element* temp = tail;
+		tail = new Element(data, nullptr, temp);
+		temp->nextptr = tail;
+	}
+	size++;
+}
+
+
+template <class T> void DLL<T>::Print() {
+	try {
+		/* Попытка вывести в консоль пустой двусвязный список */
+		if (head == nullptr) throw ("Error: Doubly linked list is empty. Unable to print.");
+	}
+	catch (const char* error) {
+		cerr << error << endl;
+		exit(-1);
+	}
+
+	Element* temp = head;
+	while (temp->nextptr != nullptr) {
+		cout << temp->data << ", ";
+		temp = temp->nextptr;
+	}
+	cout << temp->data << endl;
+}
+#endif // !DOUBLY_LINKED_LIST_H
---
+++ Programming Technology/Laboratory 2/lab_2.cpp
@@ -0,0 +1,35 @@
+﻿/* ЗАДАНИЕ:
+
+Описать двусвязный список элементов. Описать функцию удаления элемента после заданного.
+Описать функции заполнения и вывода элементов.
+
+Использовать шаблоны классов
+
+*/
+
+/* СПИСОК ИСКЛЮЧЕНИЙ:
+
+1. Попытка вывести в консоль пустой двусвязный список	:: код -1
+2. Неверный индекс для удаления элемента				:: код -2
+
+*/
+
+#include "Doubly_Linked_List.h"
+
+int main() {
+	DLL<int> intList;
+
+	intList.pushBack(2);
+	intList.pushBack(3);
+	intList.pushBack(4);
+	intList.pushFront(5);
+	intList.pushBack(7);
+	intList.Print();
+
+	intList.erase(0);
+
+	intList.Print();
+
+	cout << endl;
+	return 0;
+}
