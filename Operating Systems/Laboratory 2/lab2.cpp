a189a2c0ce3bc03917517c992841ebf15fa9a193
Andrey Semenov <61043339+Lawranio@users.noreply.github.com>
2021-05-16 23:03:44 +0300

    Add files via upload

--- Operating Systems/Laboratory 2/lab2.cpp
+++ Operating Systems/Laboratory 2/lab2.cpp
@@ -17,18 +17,21 @@ void printArray(int *array, int n) {
 
 int temp;	// for changing
 int temp2;	// for B array
-int pid;
+
+pid_t pid, wpid;
+int status = 0;
+
+const int n = 5;
+
+int A[n][n];		// start array
+int B[n][n];		// result array
+int str[n];		// for sending
+int par_str[n];	// for receiving
 
 int main() {
 
 	int fd[2];
 	int result;
-
-	const int n = 5;
-	int A[n][n];	// start array
-	int B[n][n];	// result array
-	int str[n];	// for sending
-	int par_str[n];	// for receiving
 	
 	int max;		// max element
 	int index;	// index of max element
@@ -45,25 +48,32 @@ int main() {
 	}
 
 
-	if (pipe(fd) < 0) cout << "Error: Can't create pipe" << endl << endl;
-
 	for (int i = 0; i < n; i++) {
+		if (pipe(fd) < 0) cout << "Error: Can't create pipe" << endl << endl;
 
-		if (fork() == 0) {
-			close(fd[0]);
-			cout << "Opened child - " << getpid() << endl;
-			waitpid(getpid(), NULL, 0);
-			read(fd[0], par_str, sizeof(par_str));
-			
+		pid = fork();
 
-			for (int i = 0; i < n; i++) {
-				B[temp][i] = str[i];
-			}
-			temp++;
+		if (pid < 0) {
+			cout << "Error: Can't fork" << endl;
+			exit(1);
 		}
 
+		if (pid == 0) {
+
+			//cout << "Parent received - " << endl;
+			read(fd[0], &par_str, sizeof(par_str));
+			//printArray(par_str, n);
+			//cout << endl;
+
+			for (int i = 0; i < n; i++) {
+				B[temp][i] = par_str[i];
+			}
+			temp++;
+
+			waitpid(pid, NULL, 0);
+		}
 		else {
-			close(fd[1]);
+			
 			for (int i = 0; i < n; i++) {
 				str[i] = A[temp][i];
 			}
@@ -80,12 +90,21 @@ int main() {
 			str[temp] = max;
 			str[index] = result;
 
-			cout << "Child send - ";
-			printArray(str, n);
+			//cout << "Child send - ";
+			//printArray(str, n);
 
-			write(fd[1], str, sizeof(str));
+			write(fd[1], &str, sizeof(str));
 			exit(0);
-			return 1;
+
 		}
+	}
+
+	cout << "\nDone" << endl;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << B[i][j] << " ";
+		}
+		cout << endl;
 	}
 }
